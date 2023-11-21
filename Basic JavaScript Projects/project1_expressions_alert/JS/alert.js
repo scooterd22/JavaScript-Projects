6960fb2ccbc958bef11e7aa90a076d27e3002058
Scott DiBenedetto <dibenedettoscott@gmail.com>
2023-11-20 16:51:46 -0800

    completed challenge

---
+++ Basic JavaScript Projects/project1_expressions_alert/JS/alert.js
@@ -0,0 +1,4 @@
+var sent1 = "this is the beginning of the string" // this sets sent1 to the string in quotes 
+var sent2 = " and this is the end of the string" // this does the same for the sent2 variable to what is in quotes
+
+alert(sent1 + sent2); // this concatinates them together and sends the alert which has an expression. 
