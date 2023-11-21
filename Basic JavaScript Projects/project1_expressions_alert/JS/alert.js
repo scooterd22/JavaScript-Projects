21ca6079149cd89597f1c7fb9b1752bcae3d0337
Scott DiBenedetto <dibenedettoscott@gmail.com>
2023-11-21 10:05:01 -0800

    completed project2_functions assignment

--- Basic JavaScript Projects/project1_expressions_alert/JS/alert.js
+++ Basic JavaScript Projects/project1_expressions_alert/JS/alert.js
@@ -1,4 +1,8 @@
 var sent1 = "this is the beginning of the string" // this sets sent1 to the string in quotes 
 var sent2 = " and this is the end of the string" // this does the same for the sent2 variable to what is in quotes
 
-alert(sent1 + sent2); // this concatinates them together and sends the alert which has an expression. 
+run_my_alert()
+ {
+
+    alert(sent1 + sent2); // this concatinates them together and sends the alert which has an expression. 
+}
---
+++ Basic JavaScript Projects/project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function myFunction() { //making my function
+    var sentence = "hi im"; // setting a variable equal to the string "hi im"
+    sentence += " scott"; // adding onto the variable " scott" with +=
+
+    document.getElementById("new_assignment").innerHTML = sentence; // using document to get to the html. Then using the IDby to get where
+    // thing i want to modify is in my html. Then I set it to be the value of sentence. 
+
+}
