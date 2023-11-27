47e5051d3b5d93e2b1e1fe36d0a89545b2678ea5
Scott DiBenedetto <dibenedettoscott@gmail.com>
2023-11-27 15:23:42 -0800

    adding to project 9

--- Basic JavaScript Projects/project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/project9_countdown_slideshow/JS/main.js
@@ -30,3 +30,24 @@ dots[slideindex-1].className += " active";
 
 
 }
+
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("time's up!");
+            clearTimeout(time);
+            time.innerHTML = "";
+        }
+    
+    }
+    tick();
+
+
+
+
+}
